deb6f9d77b9af9671c43bad2c480546ea9b8ecc1
kapil choudhary <kapilchoudhary0123.com>
2023-06-14 12:53:03 +0530

    Commit dated 14master/06/2023

--- 12 Mapty App: OOP, Geoloction, External Libraries, and More!/script.js
+++ 12 Mapty App: OOP, Geoloction, External Libraries, and More!/script.js
@@ -11,17 +11,61 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
-let map, mapEvent;
+// let map, mapEvent;
 
+class Workout {
+  id = (Date.now() + "").slice(-10);
+
+  constructor(coords, distance, duration) {
+    this.coords = coords;
+    this.distance = distance;
+    this.duration = duration;
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    this.pace = this.distance / this.duration;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+// const run1 = new Running([55, 15], 15, 1.5, 5);
+// const cycle1 = new Cycling([-55, 15], 5, 55, 500);
+// console.log(run1, cycle1);
 class App {
+  #map;
+  #mapEvent;
+
   constructor() {
     this.#getPosition();
+    form.addEventListener("submit", this.#newWorkout.bind(this));
+    inputType.addEventListener("change", this.#toggleElevationField);
   }
 
   #getPosition() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
-        this.#loadMap,
+        this.#loadMap.bind(this),
         function () {
           alert("Please turn on Location and reload!");
         },
@@ -33,29 +77,53 @@ class App {
   #loadMap(position) {
     const { latitude } = position.coords;
     const { longitude } = position.coords;
-    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
     const coords = [latitude, longitude];
-    map = L.map("map").setView(coords, 13);
+    this.#map = L.map("map").setView(coords, 13);
 
     L.tileLayer("http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}", {
       maxZoom: 20,
       subdomains: ["mt0", "mt1", "mt2", "mt3"],
-    }).addTo(map);
-
-    map.on("click", function (mapE) {
-      mapEvent = mapE;
-      form.classList.remove("hidden");
-      inputDistance.focus();
-    });
+    }).addTo(this.#map);
+    this.#map.on("click", this.#showForm.bind(this));
   }
 
-  #showForm() {}
+  #showForm(mapE) {
+    this.#mapEvent = mapE;
 
-  #hideForm() {}
+    form.classList.remove("hidden");
+    inputDistance.focus();
+  }
 
-  #toggleElevationField() {}
+  #hideForm() {
+    form.classList.add("hidden");
+    form.reset();
+  }
 
-  #newWorkout() {}
+  #toggleElevationField() {
+    inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+    inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+  }
+
+  #newWorkout(evt) {
+    evt.preventDefault();
+    const { lat, lng } = this.#mapEvent.latlng;
+
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          minWidth: 100,
+          maxWidth: 250,
+          autoClose: false,
+          closeOnClick: false,
+          className: "running-popup",
+        })
+      )
+      .setPopupContent(inputType.value)
+      .openPopup();
+
+    this.#hideForm();
+  }
 }
 
 const app = new App();
@@ -105,4 +173,8 @@ const app = new App();
 //
 //   form.classList.add("hidden");
 //   form.reset();
+// });
+// inputType.addEventListener("change", function () {
+//   inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+//   inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
 // });
