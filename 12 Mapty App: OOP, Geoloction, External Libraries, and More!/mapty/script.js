4ffd843b26fca7d7ecf521faef45e68e9c640210
kapil choudhary <kapilchoudhary0123.com>
2023-10-23 19:58:46 +0530

    mapty changes

--- 12 Mapty App: OOP, Geoloction, External Libraries, and More!/mapty/script.js
+++ 12 Mapty App: OOP, Geoloction, External Libraries, and More!/mapty/script.js
@@ -117,7 +117,7 @@ class App {
     const coords = [latitude, longitude];
     this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
 
-    L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+    L.tileLayer('https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
       maxZoom: 20,
       subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
     }).addTo(this.#map);
